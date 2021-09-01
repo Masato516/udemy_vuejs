73b0091b82ae90dd9e383fd47fc14f2ff9bd8bfd
Masato Yagi <Masato Yagi>
2021-09-02 01:38:55 +0900

    slotの基礎の動作確認

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <LikeHeader header-text="props経由の親からのstring">
-      <!-- <h1>slot経由のHTML</h1>
-      <h2>{{ number }}</h2> -->
+      <h1>slot経由のHTML</h1>
+      <h2>{{ number }}</h2>
     </LikeHeader>
   </div>
 </template>
